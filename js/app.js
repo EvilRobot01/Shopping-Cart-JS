08f4e7b00686f6f9f1058b3de0d8366ed048097a
Harold DRF <95robots@gmail.com>
2019-02-12 20:44:25 -0700

    Product id added

--- js/app.js
+++ js/app.js
@@ -17,5 +17,19 @@ function buyCourse(e){
 }
 
 funtion getCourseInfo(course){
+const courseInfo = {
+    image: course.querySelector('img').src,
+    title: course.querySelector('h4').textContent,
+    price: course.querySelector('.price span').textContent,
+    id: course.querySelector('a').getAttribute('data-id')
+}
+addIntoCart(courseInfo);
+}
 
+function addIntoCart(course){
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+    
+    `;
 }
